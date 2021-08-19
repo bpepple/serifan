986e2a36213848eba54aff4a21384f86d65de809
Brian Pepple <bdpepple@gmail.com>
2021-08-18 21:13:31 -0400

    Initial work on the api wrapper

--- serifan/__init__.py
+++ serifan/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+from serifan import session
+
+
+def api():
+    return session.Session()
---
+++ serifan/session.py
@@ -0,0 +1,63 @@
+import platform
+from collections import OrderedDict
+from urllib.parse import urlencode
+
+import requests
+
+from serifan import __version__, comics_list, exceptions
+
+
+class Session:
+    """
+    Session to request api endpoints
+    """
+
+    def __init__(self) -> None:
+        self.header = {
+            "User-Agent": f"Serifan/{__version__} ({platform.system()}; {platform.release()})"
+        }
+        self.api_url = "https://api.shortboxed.com/comics/v1/{}"
+
+    def call(self, endpoint, params=None):
+        """
+        Method to make request for api endpoints.
+        :param str endpoint: The endpoint to request information from.
+        :param dict params: Parameters to add to the request.
+        """
+        if params is None:
+            params = {}
+
+        cache_params = ""
+        if params:
+            ordered_params = OrderedDict(sorted(params.items(), key=lambda t: t[0]))
+            cache_params = "?{}".format(urlencode(ordered_params))
+
+        url = self.api_url.format("/".join(str(e) for e in endpoint))
+
+        try:
+            response = requests.get(url, params=params, headers=self.header)
+        except requests.exceptions.ConnectionError as e:
+            raise exceptions.ApiError("Connection error: {}".format(repr(e)))
+
+        return response.json()
+
+    def new_releases(self) -> comics_list.ComicsList:
+        """
+        Method to request a list of this weeks current new release comics.
+
+        :return: A list of :class:`Comic` objects.
+        :rtype: ComicsList
+        """
+        return comics_list.ComicsList(self.call(["new"], params={}))
+
+    def previous_releases(self) -> comics_list.ComicsList:
+        """
+        Method to request a list of the previous weeks released comics.
+
+        :return: A list of :class:`Comic` objects.
+        :rtype: ComicsList
+        """
+        return comics_list.ComicsList(self.call(["previous"], params={}))
+
+    def future_releases(self) -> comics_list.ComicsList:
+        return comics_list.ComicsList(self.call(["future"], params={}))
