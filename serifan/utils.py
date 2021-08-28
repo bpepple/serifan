5e9e8a13e9b006c55a7fe3fedd2b45b903a6e89f
Brian Pepple <bdpepple@gmail.com>
2021-08-28 16:43:18 -0400

    Add some documentation for function

--- serifan/utils.py
+++ serifan/utils.py
@@ -4,5 +4,14 @@ from typing import List
 
 
 def list_strings_to_dates(lst) -> List[date]:
+    """
+    Function that takes a list of date strings and converts them to a list of date objects
+
+    :param lst: A list of date strings
+    :type params: list of str
+
+    :return: A list of `datetime.date` objects
+    :rtype: date
+    """
     # One of the string dates in the api has a period at the end.
     return [dt.strptime(d.strip("."), "%Y-%m-%d").date() for d in lst["dates"]]
